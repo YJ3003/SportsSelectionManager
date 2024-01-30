03bac6b0e888eb79357b709d18b61eb120411699
YJ3003 <138485796+YJ3003@users.noreply.github.com>
2024-01-30 11:57:18 +0530

    Add files via upload

---
+++ stlit.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from tempCodeRunnerFile import df, sports_group, send_emails
+
+try:
+    # Create a session state
+    st.title("SPORTS SELECTION MANAGER")
+    if 'selected_sport' not in st.session_state:
+        st.session_state.selected_sport = None
+
+    # Create a select box to choose the sports group
+    st.session_state.selected_sport = st.selectbox("Enter sports", sports_group, index=0)
+
+
+    # Add a text input box with an initial value
+    sub = st.text_input("Enter your subject:", value="<subject_line>")
+    msg = st.text_input("Enter your message:", value="<message_line>")
+    sport = st.session_state['selected_sport']
+
+    # Send emails when the button is clicked
+    if st.button("Send Emails"):
+        send_emails(sport,sub, msg)
+        st.write(f"Emails to {sport} sent successfully!")
+    
+    st.write(df)
+
+
+except Exception as e:
+    st.error(f"An error occurred: {e}")
